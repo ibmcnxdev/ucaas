eba4ce00dfd31aebae025b064e3d11a606902616
vju42 <volker@juergensen.name>
2018-08-17 14:07:21 +0200

    Update via git commit --interactive

--- profilephonenumber.js
+++ profilephonenumber.js
@@ -17,24 +17,24 @@
 //
 // ==/UserScript==
 
-if(typeof(dojo) != "undefined") {
-	require(["dojo/domReady!"], function(){
+if (typeof (dojo) != "undefined") {
+    require(["dojo/domReady!"], function () {
         try {
             // utility function to let us wait for a specific element of the page to load...
-            var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
-                if(!elXpathRoot) var elXpathRoot = dojo.body();
-                if(!maxInter) var maxInter = 10000;  // number of intervals before expiring
-                if(!waitTime) var waitTime = 1;  // 1000=1 second
-                if(!elXpath) return;
+            var waitFor = function (callback, elXpath, elXpathRoot, maxInter, waitTime) {
+                if (!elXpathRoot) var elXpathRoot = dojo.body();
+                if (!maxInter) var maxInter = 10000;  // number of intervals before expiring
+                if (!waitTime) var waitTime = 1;  // 1000=1 second
+                if (!elXpath) return;
                 var waitInter = 0;  // current interval
-                var intId = setInterval( function(){
-                    if( ++waitInter<maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
+                var intId = setInterval(function () {
+                    if (++waitInter < maxInter && !dojo.query(elXpath, elXpathRoot).length) return;
 
                     clearInterval(intId);
-                    if( waitInter >= maxInter) { 
-                        console.log("**** WAITFOR ["+elXpath+"] WATCH EXPIRED!!! interval "+waitInter+" (max:"+maxInter+")");
+                    if (waitInter >= maxInter) {
+                        console.log("**** WAITFOR [" + elXpath + "] WATCH EXPIRED!!! interval " + waitInter + " (max:" + maxInter + ")");
                     } else {
-                        console.log("**** WAITFOR ["+elXpath+"] WATCH TRIPPED AT interval "+waitInter+" (max:"+maxInter+")");
+                        console.log("**** WAITFOR [" + elXpath + "] WATCH TRIPPED AT interval " + waitInter + " (max:" + maxInter + ")");
                         callback();
                     }
                 }, waitTime);
@@ -42,24 +42,21 @@ if(typeof(dojo) != "undefined") {
 
             // here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
             // before we proceed to customize the page...
-            waitFor( function(){
-			// wait until the "loading..." node has been hidden
-			
-      // here we go
-   		//	dojo.query("span.shareSome-title")[0].textContent="Willkommen beim UCaaS PoC für Sievert AG! ";
-      var span= document.getElementById("businessDetails");
-var aTag = document.createElement('a');
-aTag.setAttribute('href',webpage);
-aTag.innerHTML = "link text";
-mydiv.appendChild(aTag);
+            waitFor(function () {
+                // wait until the "loading..." node has been hidden
 
-      
-      
-      
-       	          },
-		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
-      } catch(e) {
-          alert("Exception occurred in helloWorld: " + e);
-      }
-   });
+                // here we go
+                //	dojo.query("span.shareSome-title")[0].textContent="Willkommen beim UCaaS PoC für Sievert AG! ";
+                var number = document.getElementById("businessCardDetails").children[0].textContent;
+                var aTag = document.createElement('a');
+                aTag.setAttribute('href', "TEL://"+number);
+                aTag.innerHTML = "Anrufen";
+                mydiv.appendChild(aTag);
+
+            },
+                ".lotusStreamTopLoading div.loaderMain.lotusHidden");
+        } catch (e) {
+            alert("Exception occurred in helloWorld: " + e);
+        }
+    });
 }
