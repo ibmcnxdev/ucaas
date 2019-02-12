cc6d73b0d9deec4c9ada16d3f76e11f644c4118c
vju42 <volker@juergensen.name>
2019-02-12 16:05:33 +0100

    Update meeting.js

--- ista/meeting.js
+++ ista/meeting.js
@@ -57,7 +57,7 @@ if (typeof (dojo) != "undefined") {
                     // hide header
                     document.getElementById('nav_bar_include').style.opacity=0
                     // hide caption
-                    document.getElementById('lotustext').style.opacity=0
+                    77 document.getElementById('lotustext').style.opacity=0
                     // hide footer
                     document.getElementById('lotusfooter').style.opacity=0
 
