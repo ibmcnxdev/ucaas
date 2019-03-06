26d65ece4740c4ea5accf60e8faffd46f14335de
vju42 <volker@juergensen.name>
2019-03-06 10:42:44 +0100

    Update meeting.js

--- ista/meeting.js
+++ ista/meeting.js
@@ -51,10 +51,10 @@ if (typeof (dojo) != "undefined") {
                     // wait until the "loading..." node has been hidden
 
                     // Change link to meetings phone number page
-                    var link = document.getElementById('accessListA');
-                    if (link) {
-                        link.onclick = function() { window.open(redirectTo); };
-                    };
+  //                  var link = document.getElementById('accessListA');
+  //                  if (link) {
+  //                      link.onclick = function() { window.open(redirectTo); };
+  //                  };
                     // hide header
                  //   document.getElementById('nav_bar_include').style.display="none";
                     document.getElementsByClassName('lotusTitleBar2')[0].style.display="none";
