ab979ce3498b537d1b462aa488207892648c84b0
vju42 <volker@juergensen.name>
2019-03-06 12:10:53 +0100

    Update meeting.js

--- ista/meeting.js
+++ ista/meeting.js
@@ -40,6 +40,27 @@ if (typeof (dojo) != "undefined") {
                 }, waitTime);
             };
 
+              // utility function to let us wait for a specific element of the page to load...
+            var waitFor2 = function (callback, elXpath, elXpathRoot, maxInter, waitTime) {
+                if (!elXpathRoot) var elXpathRoot = dojo.body();
+                if (!maxInter) var maxInter = 10000;  // number of intervals before expiring
+                if (!waitTime) var waitTime = 1;  // 1000=1 second
+                if (!elXpath) return;
+                var waitInter = 0;  // current interval
+                var intId = setInterval(function () {
+                    if (++waitInter < maxInter && !dojo.query(elXpath, elXpathRoot).length) return;
+
+                    clearInterval(intId);
+                    if (waitInter >= maxInter) {
+                        console.log("**** WAITFOR [" + elXpath + "] WATCH EXPIRED!!! interval " + waitInter + " (max:" + maxInter + ")");
+                    } else {
+                        console.log("**** WAITFOR [" + elXpath + "] WATCH TRIPPED AT interval " + waitInter + " (max:" + maxInter + ")");
+                        callback();
+                    }
+                }, waitTime);
+            };
+          
+            
             var redirectTo = 'https://one.ista.com/wikis/home?#!/wiki/W1537362d7845_4212_a923_7157719ea3cd/page/ONE%20-%20Telefoneinwahlnummern';
             
             var path = document.location.pathname;
@@ -69,7 +90,7 @@ if (typeof (dojo) != "undefined") {
                     
                   // new waitfor  
                     
-       waitFor(function () {             
+       waitFor2(function () {             
                  console.log("sthome audioprovision");
            
                  //var audio = document.getElementById("provisionPCA");
