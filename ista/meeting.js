498999ea2f236fca3e9f6c10371ffc33478745bf
vju42 <volker@juergensen.name>
2019-03-06 10:41:53 +0100

    Update meeting.js

--- ista/meeting.js
+++ ista/meeting.js
@@ -44,7 +44,7 @@ if (typeof (dojo) != "undefined") {
             
             var path = document.location.pathname;
             if (path.indexOf("/sthome") != -1) {
-            
+                console.log("sthome start");
                 // here we use waitFor to wait on #accesListA element
                 // before we proceed to customize the page...
                 waitFor(function () {
