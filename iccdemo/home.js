5fac6faa8b0f5a8d347f4f67233db02cc5512679
Volker <volker@juergensen.name>
2019-05-17 08:49:03 +0200

    Create home.js

---
+++ iccdemo/home.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @copyright    Copyright IBM Corp. 2019
+//
+// @name         home.js
+// @version      0.5
+// @description  Redirect home page to meeting page
+//
+// @namespace  http://ibm.com
+//
+// @author       Volker Juergensen
+//
+// @include      *://apps.collabservintegration.com/homepage/*
+//
+// @exclude
+//
+// @run-at       document-end
+//
+// ==/UserScript==
+
+
+            
+            var redirectTo = 'https://apps.ce.collabserv.com/meetings/sthome';
+            
+            var path = document.location.pathname;
+            if (path.indexOf("/homepage") != -1) {
+
+            document.location=redirectTo;
+ 
+}
