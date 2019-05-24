e3ac0a7d037df20f607ee96679fa62cc03885f7f
Volker <volker@juergensen.name>
2019-05-24 10:57:43 +0200

    Update meeting.js
    re-done

--- iccdemo/meeting.js
+++ iccdemo/meeting.js
@@ -79,16 +79,16 @@ if (typeof (dojo) != "undefined") {
                  // document.getElementById('nav_bar_include').style.display="none";
                  // remove header
                  document.getElementById('nav_bar_include').remove();
-                    console.log("sthome 1");
+                 console.log("sthome 1");
  
-                 //document.getElementsByClassName('lotusTitleBar2')[0].style.display="none";
-                 document.getElementsByClassName('lotusTitleBar2')[0].remove();
+                 document.getElementsByClassName('lotusTitleBar2')[0].style.display="none";
+                 //document.getElementsByClassName('lotusTitleBar2')[0].remove();
                     // hide caption
-                 //document.getElementsByClassName("lotusHeading")[0].style.display="none";
-                 document.getElementsByClassName("lotusHeading")[0].remove();
+                 document.getElementsByClassName("lotusHeading")[0].style.display="none";
+                 //document.getElementsByClassName("lotusHeading")[0].remove();
                       // hide footer
-                 //document.getElementsByClassName("lotusFooter")[0].style.display="none";
-                 document.getElementsByClassName("lotusFooter")[0].remove();
+                 document.getElementsByClassName("lotusFooter")[0].style.display="none";
+                 //document.getElementsByClassName("lotusFooter")[0].remove();
                    
                     // show page after all changes are done
                     
